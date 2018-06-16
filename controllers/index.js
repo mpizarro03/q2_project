ed013c0eaf754294c6b79be5c8d94adb3eb0e933
Melissa Pizarro <mpizarro03@gmail.com>
2018-06-16 16:27:50 -0600

    set up MVC routes

---
+++ controllers/index.js
@@ -0,0 +1,21 @@
+const model = require('../models/index.js')
+
+function getAll(req, res, next){
+  const data = model.getAll()
+  res.status(200).json({data})
+}
+
+function create(req, res, next){
+  const problem = model.create(req.body)
+  res.status(201).json({data: problem})
+}
+
+function login(req, res, next){
+  const teamUser = model.login(req.body)
+  res.status(200)
+}
+function query(req, res, next){
+  const search = model.query(req.body)
+  res.status(200).json({data: search})
+}
+module.exports = {getAll, create, login,query}

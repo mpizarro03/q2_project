4c56731da6468b746a6ebf1dd4e59ef61c034058
Aimee Zawacki <aimee@tna.digital>
2018-06-17 13:11:39 -0600

    added geolocation functionality, currently console logging the lat and long.

--- public/index.js
+++ public/index.js
@@ -1,55 +1,77 @@
 $(document).ready(function(){
 
   const otherTopic =
-      `<div class="input-field col s6">
-        <input placeholder="Other topic" id="type" type="text" class="validate">
-       </div>`
+    `<div class="input-field col s6">
+      <input placeholder="Other topic" id="type" type="text" class="validate">
+    </div>`
 
   const start =
-      `<div class="input-field col s12 m8 l6">
-        <div class="row">
-          <label for="textarea1">Wouldn't it be great if</label>
-          <textarea id="textarea1" class="materialize-textarea"></textarea>
-        </div>
-        <div class="row">
-          <label for="textarea1">would</label>
-          <textarea id="textarea1" class="materialize-textarea"></textarea>
-        </div>
-        <div class="row">
-          <label for="textarea1">so that</label>
-          <textarea id="textarea1" class="materialize-textarea"></textarea>
-        </div>
-        <div class="row">
-          <label for="textarea1">Would you like to share anything else with us?</label>
-          <textarea id="textarea1" class="materialize-textarea"></textarea>
-        </div>
+    `<div class="input-field col s12 m8 l6">
+      <div class="row">
+        <label for="textarea1">Wouldn't it be great if</label>
+        <textarea id="textarea1" class="materialize-textarea"></textarea>
+      </div>
+      <div class="row">
+        <label for="textarea1">would</label>
+        <textarea id="textarea1" class="materialize-textarea"></textarea>
+      </div>
+      <div class="row">
+        <label for="textarea1">so that</label>
+        <textarea id="textarea1" class="materialize-textarea"></textarea>
+      </div>
+      <div class="row">
+        <label for="textarea1">Would you like to share anything else with us?</label>
+        <textarea id="textarea1" class="materialize-textarea"></textarea>
+      </div>
+    </div>`
 
-      </div>`
+  const stop =
+    `<div class="input-field col s12 m8 l6">
+      <div class="row">
+        <label for="textarea1">It's terrible that</label>
+        <textarea id="textarea1" class="materialize-textarea"></textarea>
+      </div>
+      <div class="row">
+        <label for="textarea1">is</label>
+        <textarea id="textarea1" class="materialize-textarea"></textarea>
+        </div>
+      <div class="row">
+        <label for="textarea1">because</label>
+        <textarea id="textarea1" class="materialize-textarea"></textarea>
+      </div>
+      <div class="row">
+        <label for="textarea1">Would you like to share anything else with us?</label>
+        <textarea id="textarea1" class="materialize-textarea"></textarea>
+      </div>
+    </div>`
 
-      const stop =
-          `<div class="input-field col s12 m8 l6">
-            <div class="row">
-              <label for="textarea1">It's terrible that</label>
-              <textarea id="textarea1" class="materialize-textarea"></textarea>
-            </div>
-            <div class="row">
-              <label for="textarea1">is</label>
-              <textarea id="textarea1" class="materialize-textarea"></textarea>
-            </div>
-            <div class="row">
-              <label for="textarea1">because</label>
-              <textarea id="textarea1" class="materialize-textarea"></textarea>
-            </div>
-            <div class="row">
-              <label for="textarea1">Would you like to share anything else with us?</label>
-              <textarea id="textarea1" class="materialize-textarea"></textarea>
-            </div>
+  function success(position) {
+    let latitude  = position.coords.latitude;
+    let longitude = position.coords.longitude;
+    console.log('Latitude is ' + latitude + '° Longitude is ' + longitude + '°')
+                  // var img = new Image();
+                  // img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
+  }
 
-          </div>`
+  function error() {
+    console.log("Unable to retrieve your location")
+  }
+
+  function geoFindMe() {
+    if (!navigator.geolocation){
+            console.log("Geolocation is not supported by your browser")
+            return
+          }
+    navigator.geolocation.getCurrentPosition(success, error);
+  }
 
   $('#other').one('click', function(){
     $('#topic').append(otherTopic)
     ev.preventDefault();
+  })
+
+  $('#location').on('click', function(){
+    geoFindMe()
   })
 
   $('.viewpoint1').on('click', function(){

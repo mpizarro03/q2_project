d4e8daf0c4828a628f43a6aa7613a97e1116c2af
Melissa Pizarro <mpizarro03@gmail.com>
2018-06-18 14:51:52 -0600

    ajax call works

--- public/index.js
+++ public/index.js
@@ -97,19 +97,28 @@ $(document).ready(function(){
     let raceData = $('.race:checked').val()
     let genderData = $('.gender:checked').val()
     let topicData = $('.topic:checked').val()
-
-
     const formData = {
-      age: $('#age').val(),
-      gender: genderData,
-      race: raceData,
-      topic: topicData,
-      noun: $('#noun').val(),
-      action: $('#action').val(),
-      reason: $('#reason').val(),
-      story: $('#story').val(),
-
+      "age": $('#age').val(),
+      "gender": genderData,
+      "race": raceData,
+      "topic": topicData,
+      "noun": $('#noun').val(),
+      "action": $('#action').val(),
+      "reason": $('#reason').val(),
+      "story": $('#story').val(),
     }
     console.log(formData)
+    $.post('http://localhost:3000/', formData)
+       .then((result) => {
+         console.log("result:", result)
+       })
   })
+  //stretch goal
+  // $('form').on('submit', function(e){
+  //   e.preventDefault()
+  //   $.post('http://localhost:3000/', $(this).serialize())
+  //     .then((result) => {
+  //       $('#result').html(result)
+  //     })
+  // })
 })
